0e8e69afe5b15bb010e7ff627773c3ccaf36b0d8
SurikenGhost <88049583+SurikenGhost@users.noreply.github.com>
2022-07-30 12:26:32 -0400

    added button attributes, waiting for model to train

--- app/src/main/java/com/example/pancreaticcancerapp/MainActivity.java
+++ app/src/main/java/com/example/pancreaticcancerapp/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.pancreaticcancerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    TextView results;
+    ImageView imageView;
+    int imageSize = 24;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.camera);
+        results = findViewById(R.id.result);
+        imageView = findViewById(R.id.image);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "button pressed", Toast.LENGTH_SHORT).show();
+
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 1);
+                } else {
+                    requestPermissions(new String[] {Manifest.permission.CAMERA}, 100);
+                }
+            }
+        });
+
     }
 }
